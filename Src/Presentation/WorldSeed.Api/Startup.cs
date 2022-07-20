ae6282ac9b043964e205fc3515e0706966405f0f
Y-Kilic <CyanGithub@protonmail.com>
2022-07-20 11:39:19 +0200

    Added  Swashbuckle to help with API documentation.
    https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-6.0&tabs=visual-studio
    
    https://github.com/domaindrivendev/Swashbuckle.AspNetCore

--- Src/Presentation/WorldSeed.Api/Startup.cs
+++ Src/Presentation/WorldSeed.Api/Startup.cs
@@ -26,6 +26,7 @@ namespace WorldSeed.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,8 @@ namespace WorldSeed.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
