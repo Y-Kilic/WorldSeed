2ce0588ffd2284c79172f18613cc554df0f5d8b3
Y-Kilic <CyanGithub@protonmail.com>
2022-08-15 16:18:09 +0200

    a small test of authentication

--- Src/Presentation/WorldSeed.Api/Controllers/WeatherForecastController.cs
+++ Src/Presentation/WorldSeed.Api/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WorldSeed.Api.Controllers
@@ -18,7 +19,9 @@ namespace WorldSeed.Api.Controllers
             _logger = logger;
         }
 
+
         [HttpGet(Name = "GetWeatherForecast")]
+        [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
