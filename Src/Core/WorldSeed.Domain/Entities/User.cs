e28bbf8f7643a55db7a4dc6c19e14561c12b0e8a
Y-Kilic <CyanGithub@protonmail.com>
2022-08-15 16:54:56 +0200

    Removed unnused field

--- Src/Core/WorldSeed.Domain/Entities/User.cs
+++ Src/Core/WorldSeed.Domain/Entities/User.cs
@@ -23,7 +23,6 @@ namespace WorldSeed.Domain.Entities
         public string Gender { get; set; }
         public DateTime BirthDate { get; set; }
         public string Email { get; set; }
-        public byte[] Password { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public DateTime CreatedAt { get; set; }
