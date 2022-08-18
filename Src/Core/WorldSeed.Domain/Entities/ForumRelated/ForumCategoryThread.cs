bf3e168d1e74006860e87333f394847d33ab94dd
Y-Kilic <CyanGithub@protonmail.com>
2022-08-18 16:51:21 +0200

    Keep track who created a thread and post.

--- Src/Core/WorldSeed.Domain/Entities/ForumRelated/ForumCategoryThread.cs
+++ Src/Core/WorldSeed.Domain/Entities/ForumRelated/ForumCategoryThread.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorldSeed.Domain.Entities.CharacterRelated;
 
 namespace WorldSeed.Domain.Entities.ForumRelated
 {
@@ -10,6 +11,7 @@ namespace WorldSeed.Domain.Entities.ForumRelated
     {
         public int Id { get; set; }
         public ForumCategory ForumCategory { get; set; }
+        public Character Owner { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
