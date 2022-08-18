3963206cfa244a74bb77c859285ce77fbc24d165
Y-Kilic <CyanGithub@protonmail.com>
2022-08-18 15:23:38 +0200

    Added entity to keep track of which user belongs to what group.

---
+++ Src/Core/WorldSeed.Domain/Entities/GroupMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldSeed.Domain.Entities
+{
+    public class GroupMember
+    {
+        public int Id { get; set; }
+        public Group Group { get; set; }
+        public User User { get; set; }
+
+    }
+}
