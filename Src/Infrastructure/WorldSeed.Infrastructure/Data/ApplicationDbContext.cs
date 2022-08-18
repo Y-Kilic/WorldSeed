490289708d90c7dcb3722b73aeb5587f6be6be33
Y-Kilic <CyanGithub@protonmail.com>
2022-08-18 17:07:01 +0200

    Added entities to dbset

--- Src/Infrastructure/WorldSeed.Infrastructure/Data/ApplicationDbContext.cs
+++ Src/Infrastructure/WorldSeed.Infrastructure/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WorldSeed.Domain.Entities.CharacterRelated;
 using WorldSeed.Domain.Entities.ForumRelated;
 using WorldSeed.Domain.Entities.GroupRelated;
 using WorldSeed.Domain.Entities.UserRelated;
@@ -12,7 +13,12 @@ namespace WorldSeed.Infrastructure.Data
     {
         public DbSet<User> Users { get; set; }
         public DbSet<UserMessage> UserMessages { get; set; }
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<CharacterMessage> CharacterMessages { get; set; }
+
         public DbSet<Group> Groups { get; set; }
+
         public DbSet<Forum> Forums { get; set; }
         public DbSet<ForumCategory> ForumCategories { get; set; }
         public DbSet<ForumCategoryThread> ForumCategoriesThreads { get; set; }
