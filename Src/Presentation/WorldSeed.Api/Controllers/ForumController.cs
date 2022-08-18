0b13ad6b55bd83de00b27e5d24320c78d38166d3
Y-Kilic <CyanGithub@protonmail.com>
2022-08-18 16:51:59 +0200

    Added controllers

---
+++ Src/Presentation/WorldSeed.Api/Controllers/ForumController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorldSeed.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForumController : ControllerBase
+    {
+    }
+}
