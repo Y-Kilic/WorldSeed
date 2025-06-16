c78edf480fa1de2eb4e6884213f13453d254098d
Y-Kilic <CyanGithub@protonmail.com>
2025-06-16 19:41:42 +0200

    Refactor forum service to use parameters

--- Src/Core/WorldSeed.Application/Interfaces/Services/IForumService.cs
+++ Src/Core/WorldSeed.Application/Interfaces/Services/IForumService.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WorldSeed.Application.DTOS;
 using WorldSeed.Domain.Entities.ForumRelated;
 
 namespace WorldSeed.Application.Interfaces.Services
 {
     public interface IForumService
     {
-        bool CreateForum(CreateForumDTO createForumDTO);
-        ForumCategory CreateCategory(CreateForumCategoryDTO dto);
-        ForumCategoryThread CreateThread(CreateForumThreadDTO dto);
-        ForumCategoryThreadPost CreatePost(CreateForumPostDTO dto);
+        bool CreateForum(string name, int? groupId);
+        ForumCategory CreateCategory(int forumId, string name);
+        ForumCategoryThread CreateThread(int forumCategoryId, int ownerId, string title);
+        ForumCategoryThreadPost CreatePost(int forumCategoryThreadId, int ownerId, string content);
         Forum GetForum(int id);
         IEnumerable<ForumCategory> GetCategories(int forumId);
         IEnumerable<ForumCategoryThread> GetThreads(int categoryId);
