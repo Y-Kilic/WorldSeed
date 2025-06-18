4a27730770caf45d292a12719a6e72896b4467bf
Y-Kilic <CyanGithub@protonmail.com>
2025-06-18 15:02:18 +0200

    Extract password hashing to service

---
+++ Src/Presentation/WorldSeed.Api/Authentication Helpers/PasswordHasher.cs
@@ -0,0 +1,14 @@
+using System.Security.Cryptography;
+
+namespace WorldSeed.Api.Temp
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+    }
+}
